0de6a761a384975dd04e39d06a0b28bee806ee22
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-10 16:00:27 +0100

    lesson 212

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Movie from "./SingleMovie";
-// lesson 210/211/
+// lesson 210/211/212/
 
 function App() {
   return (

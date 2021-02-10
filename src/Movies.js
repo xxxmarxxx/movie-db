468ad20497b9cfdf13703cde0f93e56aceb41869
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-10 16:39:16 +0100

    loading movies and NO PHOTO

--- src/Movies.js
+++ src/Movies.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  // const data = useGlobalContext()
-  // console.log(data);
-  return <h2>movies component</h2>
-}
+  const { movies, isLoading } = useGlobalContext();
 
-export default Movies
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
+        console.log(poster);
+        return (
+          <Link to={`/movies/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster === "N/A" ? url : poster} alt={title} />
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
+
+export default Movies;
